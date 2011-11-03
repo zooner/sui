d9a97656268d64edfce1e2ce2ef3e5defe539910
Denis Koronchik <denis.koronchik@gmail.com>
2011-11-03 23:51:15 +0300

    added script that copyes test knowledge base into repository

--- tools/build_kb.sh
+++ tools/build_kb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 clear
+echo "Copy test knowlegde base into repository"
+cp -r -f "../test/kb" "../repo/main/test"
 echo "Build knowledge base"
 python rule_builder.py --fs_repo ../sui/kb --fs_repo_src ../repo

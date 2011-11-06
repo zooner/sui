01658da4ea32d674d200732bbec50f39a9246b32
Denis Koronchik <denis.koronchik@gmail.com>
2011-11-06 20:57:38 +0300

    fixed knowledge base build script

--- tools/build_kb.sh
+++ tools/build_kb.sh
@@ -4,4 +4,4 @@ echo "Copy test knowlegde base into repository"
 mkdir "../repo/main/test"
 cp -r -f "../test/kb" "../repo/main/test"
 echo "Build knowledge base"
-python rule_builder.py --fs_repo ../sui/kb --fs_repo_src ../repo
+python rule_builder.py --fs_repo ../bin/kb --fs_repo_src ../repo
